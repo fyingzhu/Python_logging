07225f22f7cd319f90151befa599009096f50d60
fyingzhu <fyingzhu@126.com>
2018-11-20 19:25:06 +0800

    只在日志文件记录1.0
    只在日志文件记录

---
+++ log_file.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+工程中我们使用一个名字为config.py的Python模块用来保存全局的配置，
+由于logging在工程中每个源代码文件都可能用到，
+因此我们把logging模块在config.py中生成一个实例，
+这样其它模块只需要引用这个实例就可以了。
+在其它模块中，我们使用这样的语句引用logger对象：
+# from config import logger
+"""
+
+import logging
+import logging.config
+import os
+
+# 在当前工作目录下创建Log文件夹
+path = os.getcwd()+"\\Log"
+if not os.path.exists(path):
+    os.mkdir(path)
+    
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('cisdi')
